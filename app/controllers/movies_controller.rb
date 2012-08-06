c6a53fb81c350c817ad05c4cd507fd0817dee4b5
Christian Perez <cfperez@stanford.edu>
2012-08-06 21:34:28 +0000

    modified:   app/controllers/movies_controller.rb 	modified:   app/views/movies/index.html.haml 	modified:   config/routes.rb 	deleted:    public/index.html

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
 
+  @@SORT_BY = [:title, :release_date]
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +9,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    flash.delete :notice
+    if params.include? :sort_by
+      sort_by = params[:sort_by]
+
+      if @@SORT_BY.include? sort_by.to_sym
+        @movies = Movie.all.sort {|a,b| a.send(sort_by) <=> b.send(sort_by)}
+        flash.delete :notice
+        return
+      else
+        flash[:notice] = "Can't sort movies by parameter #{sort_by}." 
+      end
+    end
+    @movies = Movie.all.sort
   end
 
   def new
